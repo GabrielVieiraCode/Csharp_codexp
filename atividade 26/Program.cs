a12e495b81eebc43ba1728b414753c5067048bc6
FIC <FIC@info.local>
2019-09-25 14:11:11 -0300

    Novas atividades

---
+++ atividade 26/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace atividade_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a idade de 10 pessoas");
+
+             int[] idade  = new int [10];
+             int contador = 0;
+             int maior=0;
+             int menor=0;
+
+
+             do{
+                 idade[contador] =int.Parse(Console.ReadLine());
+                 contador++;
+                 
+                   if(idade[contador]<=18){
+                       maior++;
+
+                 
+             }
+             else{
+                 menor++;
+                
+             }
+
+             }while(contador<=10);
+
+             System.Console.WriteLine($"maiores de idade{maior}");
+             System.Console.WriteLine($"menores {menor}");
+
+             
+
+
+            
+
+           
+
+
+              
+
+
+
+
+        }
+    }
+}
---
+++ atividade 27/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace atividade_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a nota de 10 alunos");
+            double[] nota = new double [10];
+            double[] nota2 = new double [10];
+            double[] media = new double[10];
+             int aprovado=0;
+             int reprovado=0;
+            
+           int contador = 0;
+            do{
+
+                System.Console.WriteLine($"Digite a 1° nota");
+
+                nota[contador]= double.Parse(Console.ReadLine());
+
+                System.Console.WriteLine($"Digite a 2° nota");
+
+                nota2[contador] = double.Parse(Console.ReadLine());
+
+                media [contador]= (nota[contador]+nota2[contador])/2;
+                
+                              
+                 if(media[contador]>=7){
+
+                 aprovado++;
+                }
+                else{
+                   reprovado++;
+                }
+                contador++;
+
+
+            }while(contador<10);
+
+             int contadorB = 0;
+            double somaMedia = 0;
+            while(contadorB<10){
+                somaMedia = somaMedia + media[contadorB];
+                contadorB++;
+
+            }
+             System.Console.WriteLine($"A media da sala é:{somaMedia/2} temos {aprovado}aprovados e {reprovado} reporvados");
+             
+
+        }
+    }
+}
